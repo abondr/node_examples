2c3100b8933a247bce10ac9801cb16997a799ee3
abon duttaroy <abondr@gmail.com>
2018-10-29 18:17:54 +0530

    master add file distribution

--- electron11/page01.js
+++ electron11/page01.js
@@ -11,10 +11,10 @@ let file2 = "pages//page02.html",
     file3 = "pages//page03.html";
 let filesList = [];
 let filesList2 = [];
-let startPath = "/home/abon/Documents/Technical Book";
+let startPath = "D:\\abcd2";
 //"D:\\abcd2";
-let getFiles = function(path, files) {
-    fs.readdirSync(path).forEach(function(file) {
+let getFiles = function (path, files) {
+    fs.readdirSync(path).forEach(function (file) {
         var subpath = path + '/' + file;
         if (fs.lstatSync(subpath).isDirectory()) {
             getFiles(subpath, files);
@@ -22,12 +22,12 @@ let getFiles = function(path, files) {
             var fileSize = fs.statSync(path + '/' + file).size;
             var filePath = path + '/' + file;
             filesList[fileSize] = filePath;
-            filesList2[fileSize] = filePath;
+            filesList2[fileSize] = { 'filePath': filePath, 'fileSize': fileSize };
         }
     });
 }
 
-let createWindow = function(renderProcess = win2, htmlPage = file2, attr = []) {
+let createWindow = function (renderProcess = win2, htmlPage = file2, attr = []) {
     renderProcess = new BrowserWindow(attr);
     renderProcess.loadURL(
         url.format({
@@ -42,34 +42,35 @@ let createWindow = function(renderProcess = win2, htmlPage = file2, attr = []) {
         renderProcess = null;
     });
 }
-app.on("ready", function() {
+app.on("ready", function () {
     getFiles(startPath, "*");
-    ipcMain.on("question-file-list", function(event, arguments) {
+    ipcMain.on("question-file-list", function (event, arguments) {
         eventSender = event.sender;
-        filesList.forEach(function(filePath, fileSize) {
+        filesList.forEach(function (filePath, fileSize) {
             createWindow(
                 winArr[fileSize],
                 file3, {
                     'width': 240,
                     'height': 160,
-                    'frame': false,
-                    show: false
+                    'frame': true,
+                    'show': true
                 }
             );
             eventSender.send("answer-file-list", { 'fileSize': fileSize, 'filePath': filePath });
         });
         eventSender.send("answer-file-list", "end_of_list");
     });
-
+    let fileList3 = Object.values(filesList2);
+    ipcMain.on("question-file-name", function (event, arguments) {
+        event.sender.send("answer-file-name", fileList3.shift());
+    });
     createWindow(
         win2,
         file2, {
-            'width': 1024,
-            'height': 768,
-            'frame': false
+            'width': 900,
+            'height': 750,
+            'frame': true
         }
     );
-    ipcMain.on("question-file-name", function(event, arguments) {
-        event.sender.send(filesList2.shift());
-    });
+
 });
--- electron11/page03.js
+++ electron11/page03.js
@@ -3,6 +3,7 @@ const ipcRenderer = electron.ipcRenderer;
 
 ipcRenderer.send("question-file-name", "");
 
-ipcRenderer.on("answer-file-name", function(event, arguments) {
-    console.log(arguments);
+ipcRenderer.on("answer-file-name", function (event, arguments) {
+    $("body").html("<h1>" + arguments["filePath"] + "</h1>");
+    $("body").append("<h1>" + arguments["fileSize"] + "</h1>");
 });
