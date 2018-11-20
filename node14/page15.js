8b2e27bda5d5fee412d3089ab1b11e6220fbe9e4
abon duttaroy <abondr@gmail.com>
2018-11-20 14:48:29 +0530

    master page path nevigation problem solved

--- node14/page15.js
+++ node14/page15.js
@@ -1,12 +1,15 @@
 http = require("http");
 fs = require("fs");
+const path = require('path');
 var port = 1234;
+file01 = path.join(__dirname, 'html_pages', 'page01.html');
+var myReadStream = fs.createReadStream(file01);
 server = http.createServer((request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    console.log(request.)
-    response.end("page response end");
+    myReadStream.pipe(response);
+    console.log(request.url);
 }).listen(port, "127.0.0.1", () => {
     console.log(`port ${port} is listning with 127.0.0.1`);
 });
